5295362a2cdd38ac2f693b1a672ee278f115c7d9
sakuraba-yusuke <sakuraba_yusuke@nanairo-inc.jp>
2022-02-28 15:27:08 +0900

    変数の開放処理とコメントを追加

--- main.ps1
+++ main.ps1
@@ -1,44 +1,72 @@
-﻿$input = Read-Host "フォルダーを指定してください"
-$list = Get-ChildItem -Path $input -Filter *.xlsx -Recurse
+﻿# 対象のExcelを取得
+$path = Read-Host "フォルダーまたはExcelファイルを指定してください"
+$list = Get-ChildItem -Path $path -Filter *.xlsx -Recurse -File
 
+# Excel開いて、高速化設定
 $excel = New-Object -ComObject Excel.Application
 $excel.DisplayAlerts = $false
 $excel.ScreenUpdating = $false
 $excel.EnableEvents = $false
 
+
+# ファイル処理
 foreach ($file in $list){
-    try{
-        
-        $book = $excel.Workbooks.Open($file.FullName)
-        
-        foreach ($sheet in $excel.Worksheets){
 
-            if ($sheet.Visible -ne $false){
-                $sheet.Activate()
-                $sheet.Cells.Item(1,1).Select() | Out-Null
-                $excel.ActiveWindow.Zoom = 100
-                $excel.ActiveWindow.ScrollColumn = 1
-                $excel.ActiveWindow.ScrollRow = 1
-            }
+    # ブックを開く
+    $book = $excel.Workbooks.Open($file.FullName)
+
+    # 進捗表示
+    $counter ++;
+    $total = ($list | ? { ! $_.PsIsContainer }).Count
+    $status = $counter.ToString() + "/" + $total.ToString()
+    $per = $counter / $total * 100
+            
+    Write-Progress -Activity "進捗" -Status $status -PercentComplete $per
+            
+    # 現在開いてるブックのシートに対してフォーカスをA1に移動させる等の処理をする
+    foreach ($sheet in $excel.Worksheets){
+
+        if ($sheet.Visible -ne $false){
+            $sheet.Activate()
+            $sheet.Cells.Item(1,1).Select() | Out-Null
+            $excel.ActiveWindow.Zoom = 100
+            $excel.ActiveWindow.ScrollColumn = 1
+            $excel.ActiveWindow.ScrollRow = 1
         }
+    }
 
-        $excel.Worksheets.Item(1).Activate()
+    # シートの一枚目にフォーカスを移動させる
+    $excel.Worksheets.Item(1).Activate()
 
-        $book.save()
-        $book.close()
-
-    } catch {
-        Write-Host "Error" -ForegroundColor Red
-        echo '$error[0] = ' + $error[0]
-    } 
+    # 保存
+    $book.save()
+        
+    # ブックを閉じる
+    $book.close()
 }
 
+# Excelの設定を戻して閉じる
 $excel.DisplayAlerts = $true
 $excel.ScreenUpdating = $true
 $excel.EnableEvents = $true
 $excel.Quit()
 
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($excel) | Out-Null
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($book) | Out-Null
-[System.Runtime.Interopservices.Marshal]::ReleaseComObject($sheet) | Out-Null
-[GC]::collect()
+# 変数開放
+Remove-Variable sheet
+Remove-Variable book
+Remove-Variable excel
+Remove-Variable file
+Remove-Variable path
+Remove-Variable list
+Remove-Variable counter
+Remove-Variable total
+Remove-Variable status
+Remove-Variable per
+
+[System.GC]::Collect()
+[System.GC]::WaitForPendingFinalizers()
+[System.GC]::Collect()
+
+1|%{$_} > $null
+[System.GC]::Collect()
+
